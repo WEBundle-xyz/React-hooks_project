8583cda95416f9bee808e7d47edb02c37cb71784
STEF G <stefano@stefs-mbp.mynetworksettings.com>
2022-02-18 14:33:07 -0500

    Sending Http requests to Firebase

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 [2 - useState and State updating ](../../tree/dfb7cdb3614b40af7b71b08fb542b64b69bc27ed/)
 
-[3 - Empty ](../../tree//)
+[3 - Adding and Removing items on/from the list ](../../tree/0dcd5db2b477ecca899a40a34db44a76520133e9/)
 
 [4 - Empty ](../../tree//)
 
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,10 +8,23 @@ const Ingredients = () => {
   const [userIngrediets, setUserIngredients] = useState([]);
 
   const addIngredientHandler = (ingredient) => {
-    setUserIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(
+      'https://reacthttppostrequests-default-rtdb.firebaseio.com/ingredients.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const removeIngredientHandler = (ingredientId) => {
