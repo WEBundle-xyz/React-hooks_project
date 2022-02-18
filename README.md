dfb7cdb3614b40af7b71b08fb542b64b69bc27ed
STEF G <stefano@stefs-mbp.mynetworksettings.com>
2022-02-18 14:06:43 -0500

    useState and State updating

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### React Hooks
 
-[1 - React Hooks - Project Setup ](../../tree//)
+[1 - React Hooks - Project Setup ](../../tree/2f413d6490751299eae3f3919f729795ac4ecadc/)
 
 [2 - Empty ](../../tree//)
 
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngrediets, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <IngredientForm />
+    <div className='App'>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
+        <IngredientList ingredients={userIngrediets} onRemoveItem={() => {}} />
         {/* Need to add list here! */}
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
